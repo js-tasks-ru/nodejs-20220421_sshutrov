07cd7cc6a09300dea7dce5db4f1b082bfc94a2b8
Sergei Shutrov <sshutrov@gmail.com>
2022-06-13 20:12:23 +0500

    Task 5 solved

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -2,17 +2,38 @@ const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
 
+var x = 0;
+
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+const users = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise((resolve,reject) => {
+        users.add(resolve);
+        ctx.res.on('close', () => {
+            users.delete(resolve);
+            resolve();
+        });
+    });
+    
+    ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+    const {message} = ctx.request.body;
+    if (!message) {
+        return ctx.throw(400, 'Empty messages not allowed!')
+    }
+    users.forEach((resolve => resolve(message)));
+    ctx.body = 'Message sended!'
 });
+
+
 
 app.use(router.routes());
 
