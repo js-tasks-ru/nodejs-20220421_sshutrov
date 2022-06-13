3b94d3268d12409549278fa45bce16798ed8619e
Sergei Shutrov <sshutrov@gmail.com>
2022-06-13 20:13:54 +0500

    Task 6.2 solved

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,13 @@
+const Category = require('../models/Category');
+const MapCategory = require('../mappers/category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const CategoryList = [];
+  const categories = await Category.find();
+
+  categories.forEach(category => {
+    CategoryList.push(MapCategory(category));
+  });
+  
+  ctx.body = {categories: CategoryList};
 };
